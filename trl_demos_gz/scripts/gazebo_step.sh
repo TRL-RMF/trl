f032d51abb30d2df1e2447bf5bdef81f06a9afd4
samuelrawrs <samuelangdj@gmail.com>
2022-04-14 14:48:53 +0800

    updated step_size changer

--- trl_demos_gz/scripts/gazebo_step.sh
+++ trl_demos_gz/scripts/gazebo_step.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sed -i '/<world name="world">/r /home/ubuntu/trl_demos_ws/src/trl/trl_demos_gz/scripts/increase_gazebo_step' /home/ubuntu/trl_demos_ws/install/trl_demos_maps/share/trl_demos_maps/maps/trl/trl.world
-echo "world file step size edited"
+physicspath=`dirname "$0"`/increase_gazebo_step
+installpath=$(builtin cd `dirname "$0"`; cd ../../../../install/trl_demos_maps/share/trl_demos_maps/maps/trl; pwd)/trl.world
+echo installpath: $installpath
+echo physicspath: $physicspath
+if grep -Ff $installpath $physicspath 
+then
+    echo "world file step size already set"
+else
+    sed -i '/<world name="world">/r '$physicspath'' $installpath
+    echo "world file step size edited"
+fi
 
-exit 0
